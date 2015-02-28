85d83b969a6ed9e80b834e65b450c731068567dd
Jiahao Li <isundaylee.reg@gmail.com>
2015-02-28 14:29:58 -0500

    Got a frame!

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from screen_controller import ScreenController
 parser = CoordinateParser(0.5, 0.5)
 print parser.parse(0.4, 0.4, 0.4)
 
-controller = ScreenController(0.5, 0.5)
+controller = ScreenController(0.5, 1.0)
 controller.run()
--- screen_controller.py
+++ screen_controller.py
@@ -40,38 +40,129 @@ class ScreenController(object):
     glut.glutDisplayFunc(self.__display)
     glut.glutKeyboardFunc(self.__keyboard)
 
-    gl.glClearColor(0, 0, 0, 1)
+    gl.glClearColor(1, 1, 1, 1)
     gl.glShadeModel(gl.GL_SMOOTH)
     gl.glEnable(gl.GL_CULL_FACE)
     gl.glEnable(gl.GL_DEPTH_TEST)
     gl.glEnable(gl.GL_LIGHTING)
 
-    lightPosition = [10, 4, 10, 1]
-    lightColor = [0.8, 1.0, 0.8, 1.0]
+    self.quadric = glu.gluNewQuadric()
+    glu.gluQuadricNormals(self.quadric, glu.GLU_SMOOTH)
+    glu.gluQuadricTexture(self.quadric, gl.GL_TRUE)
+
+    lightPosition = [self.side / 2, self.height / 2, self.side / 3, 0.05]
+    lightColor = [10.0 / 255, 124.0 / 255, 1, 1]
 
     gl.glLightfv(gl.GL_LIGHT0, gl.GL_POSITION, lightPosition)
     gl.glLightfv(gl.GL_LIGHT0, gl.GL_DIFFUSE, lightColor)
-    gl.glLightf(gl.GL_LIGHT0, gl.GL_CONSTANT_ATTENUATION, 0.1)
+    gl.glLightf(gl.GL_LIGHT0, gl.GL_CONSTANT_ATTENUATION, 0.01)
     gl.glLightf(gl.GL_LIGHT0, gl.GL_LINEAR_ATTENUATION, 0.05)
+    gl.glColorMaterial(gl.GL_FRONT_AND_BACK, gl.GL_AMBIENT)
+    gl.glEnable(gl.GL_COLOR_MATERIAL)
     gl.glEnable(gl.GL_LIGHT0)
 
+
     gl.glMatrixMode(gl.GL_PROJECTION)
-    glu.gluPerspective(40, 1, 1, 40)
+    glu.gluPerspective(20, 1, 1, 40)
     gl.glMatrixMode(gl.GL_MODELVIEW)
-    glu.gluLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0)
+    glu.gluLookAt(
+      self.side / 2, self.height / 2, 3,
+      self.side / 2, self.height / 2, 0,
+      0, 1, 0
+    )
     gl.glPushMatrix()
 
     glut.glutMainLoop()
 
+  # Translate from reallife coordinates to OpenGL coordinates
+  def tr(self, x, y, z):
+    return (x, z, -y)
+
+  # Translate to reallife coordinates from OpenGL coordinates
+  def fr(self, x, y, z):
+    return (x, z, -y)
+
+  def __draw_cylinder(self, p1, p2, radius=0.005):
+    p1, p2 = self.tr(p1[0], p1[1], p1[2]), self.tr(p2[0], p2[1], p2[2])
+
+    dx = float(p2[0] - p1[0])
+    dy = float(p2[1] - p1[1])
+    dz = float(p2[2] - p1[2])
+
+    r = math.sqrt(dy ** 2 + dz ** 2)
+    length = math.sqrt(dx ** 2 + dy ** 2 + dz ** 2)
+
+    if r == 0:
+      x_angle = 0
+    else:
+      x_angle = math.atan2(-dy / r, dz / r)
+    y_angle = math.atan2(dx / length, r / length)
+
+    gl.glPushMatrix()
+
+    gl.glTranslatef(p1[0], p1[1], p1[2])
+    gl.glRotatef(math.degrees(x_angle), 1, 0, 0)
+    gl.glRotatef(math.degrees(y_angle), 0, 1, 0)
+
+    glu.gluCylinder(self.quadric, radius, radius, length, 32, 32)
+
+    gl.glPopMatrix()
+
+  def __draw_sphere(self, p1, radius=0.01):
+    p1 = self.tr(p1[0], p1[1], p1[2])
+
+    gl.glPushMatrix()
+
+    gl.glTranslatef(p1[0], p1[1], p1[2])
+
+    glu.gluSphere(self.quadric, radius, 32, 32)
+
+    gl.glPopMatrix()
+
   def __display(self):
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+
+    # Drawing the axes
+    gl.glBegin(gl.GL_LINES)
+
+    # gl.glVertex3f(0.0, 0.0, 0.0)
+    # gl.glVertex3f(10.0, 0.0, 0.0)
+
+    # gl.glVertex3f(0.0, 0.0, 0.0)
+    # gl.glVertex3f(0.0, 10.0, 0.0)
+
+    # gl.glVertex3f(0.0, 0.0, 0.0)
+    # gl.glVertex3f(0.0, 0.0, 10.0)
+
+    gl.glEnd()
+
     # Drawing the framework
 
-    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
-    gl.glPushMatrix()
-    color = [1.0, 0, 0, 1]
-    gl.glMaterialfv(gl.GL_FRONT, gl.GL_DIFFUSE, color)
-    glut.glutSolidSphere(2, 20, 20)
-    gl.glPopMatrix()
+    gl.glColor3f(1.0, 0.0, 0.0)
+
+    # Bottom triangle
+    self.__draw_cylinder((0, 0, 0), (0.5 * self.side, math.sqrt(0.75) * self.side, 0))
+    self.__draw_cylinder((self.side, 0, 0), (0.5 * self.side, math.sqrt(0.75) * self.side, 0))
+    self.__draw_cylinder((0, 0, 0), (self.side, 0, 0))
+
+    # Top triangle
+    self.__draw_cylinder((0, 0, self.height), (0.5 * self.side, math.sqrt(0.75) * self.side, self.height))
+    self.__draw_cylinder((self.side, 0, self.height), (0.5 * self.side, math.sqrt(0.75) * self.side, self.height))
+    self.__draw_cylinder((0, 0, self.height), (self.side, 0, self.height))
+
+    # Sides
+    self.__draw_cylinder((0, 0, 0), (0, 0, self.height))
+    self.__draw_cylinder((self.side, 0, 0), (self.side, 0, self.height))
+    self.__draw_cylinder((0.5 * self.side, math.sqrt(0.75) * self.side, 0), (0.5 * self.side, math.sqrt(0.75) * self.side, self.height))
+
+    # Decorative spheres
+
+    self.__draw_sphere((0, 0, 0))
+    self.__draw_sphere((self.side, 0, 0))
+    self.__draw_sphere((0.5 * self.side, math.sqrt(0.75) * self.side, 0))
+    self.__draw_sphere((0, 0, self.height))
+    self.__draw_sphere((self.side, 0, self.height))
+    self.__draw_sphere((0.5 * self.side, math.sqrt(0.75) * self.side, self.height))
 
     glut.glutSwapBuffers()
 
